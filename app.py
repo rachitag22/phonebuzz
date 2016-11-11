732c6ecab7dfd663a94563dd292473117b43dfee
Rachit Agarwal <rachitagarwal0202@gmail.com>
2016-11-11 06:42:18 +0000

    Phase 1 & 2 complete. Still improving #2

--- app.py
+++ app.py
@@ -1,11 +1,77 @@
 import os
-from flask import Flask
+import twilio.twiml
+from twilio import twiml
+from twilio.util import RequestValidator
+from twilio.rest import TwilioRestClient
+from twilio import TwilioRestException
+from flask import Flask, request
 from flask import render_template
+from config import *
+import hmac
+import hashlib
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+def request_check():
+    print("todo")
+
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    outbound()
     return render_template("index.html")
 
+@app.route("/welcome", methods=['GET', 'POST'])
+def welcome():
+    request_check()
+    resp = twilio.twiml.Response()
+    
+    with resp.gather(finishOnKey="#", action="/handle-number", method="POST") as g:
+        message = "Welcome to PhoneBuzz! Please enter any sequence of numbers, followed by the pound sign."
+        g.say(message, voice="woman")
+
+    return str(resp)
+
+@app.route("/outbound", methods=['GET', 'POST'])
+def outbound():
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token  = TWILIO_AUTH_TOKEN
+    call_from = TWILIO_CALLER_ID
+    call_to = "+13012813692"
+        
+    client = TwilioRestClient(account_sid, auth_token)
+    url = "https://lendup-challenge-phonebuzz-rachitag22.c9users.io/welcome"
+    toNum = "+13012813692"
+    fromNum = "+13012311851"
+    
+    call = client.calls.create(url=url, to=toNum, from_=fromNum)
+    print(call.sid)
+
+@app.route("/handle-number", methods=['GET', 'POST'])
+def handle_number():
+    
+    number_string = request.values.get('Digits', None)
+    resp = twilio.twiml.Response()
+    message = "..."
+    
+    for ch in number_string:
+    	message += " "
+    	num = int(ch)
+    	
+    	if num % 3 == 0 and num % 5 == 0:
+    		message += "Fizz Buzz"
+    	elif num % 3 == 0:
+    		message += "Fizz"
+    	elif num % 5 == 0:
+    		message += "Buzz"
+    	else:
+    		message += str(num)
+    	
+    	message += "..."
+    	
+    message += "......Thank you for using PhoneBuzz!"
+    resp.say(message, voice="woman")
+    return str(resp)
+    
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
--- config.py
+++ config.py
@@ -2,12 +2,9 @@
 Configuration Settings
 '''
 
-TWILIO_ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
-TWILIO_AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
-TWILIO_CALLER_ID = "+17778889999"
-
 import os
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
-TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
-TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
+
+TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_CALLER_ID = os.environ["TWILIO_CALLER_ID"]
+TWILIO_APP_SID = os.environ["TWILIO_APP_SID"]
